6bac655c467cba1a388bcb15eb10e55064d660af
catmanti <somaratne@yahoo.com>
2022-04-05 19:07:42 +0530

    initial Commit

---
+++ tutorial/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!fy$jercep*#s9!00b#-92!+*^kwnh0#&_sq*bzz(5$x(3__0r'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
